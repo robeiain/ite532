d6f59cf753325900c1ac4a5d73837477cf5877ae
tallmantim <tallmantim@gmail.com>
2022-10-10 14:58:24 +1100

    Create aws-ubuntu-20.04LTS-Docker-install.sh

---
+++ aws-ubuntu-20.04LTS-Docker-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install -qyy docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo service docker start
